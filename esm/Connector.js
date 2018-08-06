713781acede75e51c357eec1b297afc7a5ce6caa
Thiago Cardoso <thiago.figueredo@cesar.org.br>
2018-08-06 12:18:03 -0300

    Add operations placeholders

--- esm/Connector.js
+++ esm/Connector.js
@@ -1,4 +1,44 @@
 class Connector {
+  async start() { // eslint-disable-line no-empty-function
+  }
+
+  async addDevice(device) { // eslint-disable-line no-empty-function,no-unused-vars
+  }
+
+  async removeDevice(id) { // eslint-disable-line no-empty-function,no-unused-vars
+  }
+
+  async listDevices() { // eslint-disable-line no-empty-function,no-unused-vars
+  }
+
+  // Device (fog) to cloud
+
+  async publishData(id, data) { // eslint-disable-line no-empty-function,no-unused-vars
+  }
+
+  async updateSchema(id, schema) { // eslint-disable-line no-empty-function,no-unused-vars
+  }
+
+  async updateProperties(id, properties) { // eslint-disable-line no-empty-function,no-unused-vars
+  }
+
+  // Cloud to device (fog)
+
+  // cb(event) where event is { id, config: {} }
+  onConfigUpdated(cb) { // eslint-disable-line no-empty-function,no-unused-vars
+  }
+
+  // cb(event) where event is { id, properties: {} }
+  onPropertiesUpdated(cb) { // eslint-disable-line no-empty-function,no-unused-vars
+  }
+
+  // cb(event) where event is { id, sensorId }
+  onDataRequested(cb) { // eslint-disable-line no-empty-function,no-unused-vars
+  }
+
+  // cb(event) where event is { id, sensorId, data }
+  onDataUpdated(cb) { // eslint-disable-line no-empty-function,no-unused-vars
+  }
 }
 
 export { Connector }; // eslint-disable-line import/prefer-default-export
